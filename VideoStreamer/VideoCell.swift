97d83cda370861831674eafe719ede17041c138f
Sidharth J Dev <sid.j.dev@gmail.com>
2024-12-12 14:05:34 +0530

    Created Cell

---
+++ VideoStreamer/VideoCell.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCell.swift
+//  VideoStreamer
+//
+//  Created by Sidharth J Dev on 12/12/24.
+//
+
+import UIKit
+
+class VideoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- VideoStreamer/ViewController.swift
+++ VideoStreamer/ViewController.swift
@@ -8,16 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let data = VideoStreamerUtils.loadCommentsData() {
-            let comments = data.comments
-            comments.forEach { print($0.comment) }
-        }
+//        if let data = VideoStreamerUtils.loadCommentsData() {
+//            let comments = data.comments
+//            comments.forEach { print($0.comment) }
+//        }
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: "videoCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoCell", for: indexPath) as? VideoCell else {
+                fatalError("Unable to dequeue VideoCell with identifier: videoCell")
+            }
+           
+        cell.backgroundColor = .red
+           
+            
+            return cell
+    }
+            
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        }
+}
+            
