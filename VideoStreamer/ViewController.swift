c1c11e5c54551e8839071d84019219724937b991
Sidharth J Dev <sid.j.dev@gmail.com>
2024-12-12 13:13:02 +0530

    Initial Commit

---
+++ VideoStreamer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreamer
+//
+//  Created by Sidharth J Dev on 12/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
